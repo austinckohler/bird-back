61875c0a6a1df194207ceb40b3eb14ccae754190
austinckohler <austinckohler@gmail.com>
2020-05-17 11:45:04 -0600

    seeded data confirmed crud actions work in postman

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Bird.create(species: "crow", age: 6, size: "medium")
+Bird.create(species: "eagle", age: 2, size: "large")
+Bird.create(species: "finch", age: 5, size: "large")
+Bird.create(species: "robin", age: 1, size: "small")
+Bird.create(species: "owl", age: 14, size: "medium-large")
+Bird.create(species: "bluebird", age: 8, size: "small-ish")
+Bird.create(species: "cardinal", age: 7, size: "small")
