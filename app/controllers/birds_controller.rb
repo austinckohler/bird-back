7ba1b8facdf9eb4147e153beb94ab1a68063897e
austinckohler <austinckohler@gmail.com>
2020-05-17 11:30:56 -0600

    added crud actions to controller

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,2 +1,44 @@
 class BirdsController < ApplicationController
+
+    before_action :find_bird_by_id, only: [:show, :update, :destroy]
+
+    def index
+        @birds = Bird.all
+
+        render json: @birds
+    end
+
+    def show
+        render json: @bird
+    end
+
+    def create
+        @bird = Bird.create(bird_params)
+        render json: @bird
+    end
+
+    def update
+        @bird.update(bird_params)
+
+        render json: @bird
+    end
+
+    def destroy
+        @bird.destroy
+
+        redirect_to action: "index"
+    end
+
+    private
+
+    def find_bird_by_id
+        @bird = Bird.find(params[:id])
+
+        render json: @bird
+    end
+
+    def bird_params
+        params.require(:bird).permit(:species, :age, :size)
+    end
+
 end
